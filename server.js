5fb6c795b6f215b2dd48b3fceeae0cb76789ca28
enestekin <zenestekin@gmail.com>
2022-09-16 00:05:26 +0300

    build front-end app

--- server.js
+++ server.js
@@ -2,16 +2,22 @@ import express from 'express';
 import productsRouter from './routes/productsRoutes.js';
 import basketRouter from './routes/basketRoutes.js';
 
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
+app.use(express.static(path.resolve(__dirname, './client/build')));
+app.use(express.json());
 app.use('/api/v1/products', productsRouter);
 app.use('/api/v1/basket', basketRouter);
 
-app.get('/', (req, res) => {
-  res.send('hello world');
+app.get('*', (req, res) => {
+  res.seendFile(path.resolve(__dirname, './client/build', 'index.html'));
 });
 
 app.listen(PORT, () => {
